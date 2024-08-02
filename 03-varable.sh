b7719da00fa1041a223d12e38dd8456208e3f16d
roshniv6 <roshniaishu6@gmail.com>
2024-08-02 12:51:53 +0530

    added 1st script

--- 03-varable.sh
+++ 03-varable.sh
@@ -4,7 +4,7 @@ echo "please enter your user name:: "
 read  -s USERNAME 
 echo "please enter your password:: "
 read  -s PASSWORD 
-echo "user name is $USERNAME and password $PASSWORD" # just doing this for understanding purpose don do it real world
+echo "user name is :: $USERNAME and password is :: $PASSWORD" # just doing this for understanding purpose don do it real world
 
 
  
