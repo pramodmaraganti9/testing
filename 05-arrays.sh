c2b873eae1f2300402334ed0aae4338231eea44d
roshniv6 <roshniaishu6@gmail.com>
2024-08-03 05:40:03 +0530

    added 1st script

--- 05-arrays.sh
+++ 05-arrays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 FROUTS=("apple" "mango" "banana")
-echo "this is an array :: ${FROUTS[@]}"
+echo "all the array values:: ${FROUTS[@]}"
 echo "only particular array:: ${FROUTS[1]}"
 echo "first array value:: ${FROUTS[0]}"
