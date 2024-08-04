2629582872f8eb9d31118525bcdd3b5f6aa84789
roshniv6 <roshniaishu6@gmail.com>
2024-08-04 21:26:14 +0530

    added code

---
+++ 09-logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+       ID=$(id -u)
+
+  validate(){
+        if  [ $? -ne 0 ]
+            then
+               echo "Error: $2 erroe"
+               exit 1
+            else
+               echo " $2 Instalaing is sucess "
+           fi
+
+  }
+        if [ $ID -ne 0 ]
+            then 
+                echo "error :: please run this script with root access"
+            else
+                echo "you are root  user"
+        fi    
+
+    yum install mysql -y
+    validate $? " installing My sql "
+
+    yum install git -y
+    validate $?  "installing GIT "
+    
