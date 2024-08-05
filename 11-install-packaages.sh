a725552ead5cc807d85e08784234d8fbb7385f6d
roshniv6 <roshniaishu6@gmail.com>
2024-08-05 13:58:49 +0530

    new code is adding

--- 11-install-packaages.sh
+++ 11-install-packaages.sh
@@ -2,9 +2,12 @@
 
 
 ID=$(id -u)
+ R="\e[31m"
+ G="\e[32m"
+ N="\e[0m"
 if [ $ID -ne 0 ]
             then 
-                echo "error :: please run this script with root access"
+                echo -e " $R error :: please run this script with root access $N"
             else
                 echo "you are root  user"
         fi
