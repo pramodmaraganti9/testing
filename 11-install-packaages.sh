25b5f15a0214d3e4861645eb0d809a0bc80af188
roshniv6 <roshniaishu6@gmail.com>
2024-08-05 15:23:42 +0530

    new code is adding

--- 11-install-packaages.sh
+++ 11-install-packaages.sh
@@ -4,7 +4,19 @@
 ID=$(id -u)
  R="\e[31m"
  G="\e[32m"
+ Y="\e[33m"
  N="\e[0m"
+ TIMESTAMP=$(date +%F-%H-%M-%S)
+ LOGFILE="/temp/$0-$TIMESTAMP"
+ echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+ VALIDATE (){
+    if [$1 -ne 0]
+    then 
+    echo -e "$2 ..... $R failed $N"
+    else
+    echo -e "$2 ......$G sucess $N"
+ }
 if [ $ID -ne 0 ]
             then 
                 echo -e " $R error :: please run this script with root access $N"
@@ -13,4 +25,15 @@ if [ $ID -ne 0 ]
                 echo "you are root  user"
         fi
 
-    echo "all arguments are passed::  $@"
+    for packages in $@
+do
+    yum list installed $packages &>> $LOGFILE
+    if ($? -ne 0)
+    then 
+        yum install $packages -y &>> $LOGFILE
+        VALIDATE $? "instalation of $packages "     
+    else
+        echo -e" package is already installed .. $Y skypi $N "
+    fi
+
+done 
