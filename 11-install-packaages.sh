96427cdd026518908d82268b21faf30c2d2db565
roshniv6 <roshniaishu6@gmail.com>
2024-08-05 15:28:45 +0530

    new code is adding

--- 11-install-packaages.sh
+++ 11-install-packaages.sh
@@ -17,7 +17,9 @@ ID=$(id -u)
     echo -e "$2 ..... $R failed $N"
     else
     echo -e "$2 ......$G sucess $N"
+    fi
  }
+
 if [ $ID -ne 0 ]
             then 
                 echo -e " $R error :: please run this script with root access $N"
